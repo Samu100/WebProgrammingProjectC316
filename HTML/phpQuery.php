0c392bef0546479953ef9fdb0889873029d60ed8
mjkj720 <90330618+mjkj720@users.noreply.github.com>
2021-12-07 23:15:04 -0500

    php

--- HTML/phpQuery.php
+++ HTML/phpQuery.php
@@ -1,23 +1,13 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
- 
-// Attempt create database query execution
-$sql = "CREATE DATABASE demo";
-if(mysqli_query($link, $sql)){
-    echo "Database created successfully";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
- 
-// Close connection
-mysqli_close($link);
+	$user = 'root';
+	$password = '';
+	$db = 'Sculptures';
+
+	$db = new mysqli('localhost', $user, $password, $db) or die("Unable to connect");
+
+	echo "good work";
+
+
 ?>
 
 
