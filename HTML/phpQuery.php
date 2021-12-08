fb430a1a84045782c3c7e9f8248710020ca7f23b
mjkj720 <90330618+mjkj720@users.noreply.github.com>
2021-12-07 22:56:38 -0500

    php

--- HTML/phpQuery.php
+++ HTML/phpQuery.php
@@ -1,5 +1,31 @@
 <?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt create database query execution
+$sql = "CREATE DATABASE demo";
+if(mysqli_query($link, $sql)){
+    echo "Database created successfully";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
 
+
+
+
+
+<?php
+/*
 $servername = 'localhost';
 $username = 'username';
 $password = 'password';
@@ -14,7 +40,7 @@ $sql = "CREATE DATABASE artDB";
 if($db->query($sql) === TRUE){
 	echo ("created");
 }
-/*
+
 mysqli_select_db("artDB", $db);
 $sql = "CREATE TABLE Sculptures
 (
